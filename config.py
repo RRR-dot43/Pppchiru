6ae2eadbef33967be269e75cb8893db4ba83fb1d
RRR-dot43 <ruhilrohit10@gmail.com>
2025-06-08 06:57:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 import os
 
-API_ID = os.environ.get("API_ID", "26468828")
+API_ID = os.environ.get("API_ID", "25967358")
 
-API_HASH = os.environ.get("API_HASH", "4693513c08d1ac6af15f95b116c29478")
+API_HASH = os.environ.get("API_HASH", "10a5a31171dfdc323efdbcf84a8fb016")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7871041512:AAGhSJDGrJRFJrnAcoEcZIXi8KULRIJi0lQ")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER", 7827463899))
+#OWNER = int(os.environ.get("OWNER", 7733608738))
 
-#LOG = -1002159628443,
+#LOG = -1002896206723,
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[7827463899]
+    #ADMINS=[7733608738]
     #for x in (os.environ.get("ADMINS", "7827463899").split()):
         #ADMINS.append(int(x))
 #except ValueError:
